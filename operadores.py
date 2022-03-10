3f5c8ed8481ad418c7b2823318b259e7b7f2ef17
Fredy Yela <fredyyela071@gmail.com>
2022-03-09 23:42:09 -0500

    Uso de operadores

---
+++ operadores.py
@@ -0,0 +1,58 @@
+print("suma:")
+
+numero_uno = 5
+numero_dos = 4
+
+resultado = numero_uno + numero_dos
+print("El resultado de la suma es: " + str(resultado))
+
+print("resta:")
+
+numero_uno = 5
+numero_dos = 4
+
+resultado = numero_uno - numero_dos
+print("El resultado de la resta es: " + str(resultado))
+
+print("multiplicación:")
+
+numero_uno = 5
+numero_dos = 4
+
+resultado = numero_uno * numero_dos
+print("El resultado de la Multiplicación es: " + str(resultado))
+
+print("exponente:")
+
+numero_uno = 2
+exponete = 5
+
+resultado = numero_uno ** exponete
+print("El resultado del exponente es: " + str(resultado))
+
+
+print("divición:")
+
+numero_uno = 4
+numero_dos = 2
+
+resultado = numero_uno / numero_dos
+print("El resultado de la divición es: " + str(resultado))
+
+
+print("módulo:")
+
+numero_uno = 30
+numero_dos = 8
+
+resultado = numero_uno % numero_dos
+print("El resultado del modulo es: " + str(resultado))
+
+
+print("divición entera:")
+
+numero_uno = 4
+numero_dos = 2
+
+resultado = numero_uno // numero_dos
+print("El resultado de la divición entera es: " + str(resultado))
