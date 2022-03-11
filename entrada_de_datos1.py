bec3f6ab4b068594891fd9cbb4310b908c968abb
Fredy Yela <fredyyela071@gmail.com>
2022-03-10 23:58:24 -0500

    Como aceptar la entrada de datos desde teclado

---
+++ entrada_de_datos1.py
@@ -0,0 +1,9 @@
+palabra = input("Introduce una palabra: ")
+num_int = int(input("Introduce un número entero: "))
+nume_float = float(input("Introduce un número flotante: "))
+numero_complex = complex(input("Introduce un número compejo: "))
+
+print("String: ", palabra)
+print("Entero: ", num_int)
+print("Flotante: ", nume_float)
+print("Número complejo: ", numero_complex)
