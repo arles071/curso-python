f3ef9d4d0ecc65375bd7e8dee1ae7d093ea93e0f
Fredy Yela <fredyyela071@gmail.com>
2022-03-12 17:07:22 -0500

    concatenacion con f-strings

---
+++ f-strings.py
@@ -0,0 +1,20 @@
+#Ejemplo1
+print(f"El resultado de la suma de 4 + 1 = {4+1}")
+
+
+#Ejemplo2
+nombre = "Fredy"
+estatura = 1.80
+edad = 22
+
+print(f"Hola {nombre} tienes {edad} años y mides {estatura} centímetros")
+
+
+#Ejemplo3
+nombre = input("¿Cúal es tu nombre?: ")
+num_uno = int(input("Introduce un número: "))
+num_dos = int(input("Introduce un segundo número: "))
+
+print(f"Hola {nombre} el resultado de {num_uno} + {num_dos} es: {num_uno + num_dos}")
+
+print("\n Fin.")
