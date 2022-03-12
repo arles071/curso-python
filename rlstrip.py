e0d61ac90f66a5299518efe3cfdcb8b442c0b970
Fredy Yela <fredyyela071@gmail.com>
2022-03-12 17:24:19 -0500

    uso del metodo rlstrip

---
+++ rlstrip.py
@@ -0,0 +1,14 @@
+#Método rstrip
+cadena = "\tHola Fredy\n"
+print(cadena)
+
+cadena = cadena.rstrip("s yHo\t\n")
+print(cadena)
+
+
+#Método lstrip
+cadena = "\tHola Fredy\n"
+print(cadena)
+
+cadena = cadena.lstrip("H\t\n")
+print(cadena)
