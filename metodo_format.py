f2679f42dfe90bcf5a19060d358c1ac36d4a7df6
Fredy Yela <fredyyela071@gmail.com>
2022-03-12 17:02:06 -0500

    otra alternativa para el usu del metodo format

--- metodo_format.py
+++ metodo_format.py
@@ -4,3 +4,5 @@ edad = 27
 print("Hola {} tiene {} años.".format(nombre, edad))
 
 print("Hola {nombre} tiene {edad} años.".format(nombre="Fredy", edad = 27))
+
+print("Hola {1} tiene {0} años.".format(nombre, edad))
